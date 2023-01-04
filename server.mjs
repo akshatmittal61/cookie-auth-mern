73c959aac326ac5bfb60c7be46eef2416e958faa
Akshat Mittal <akshatmittal2506@gmail.com>
2023-01-04 18:26:51 +0530

    created configs for server

--- server.mjs
+++ server.mjs
@@ -4,6 +4,7 @@ import { config } from "dotenv";
 import { PORT } from "./config/index.mjs";
 
 import apiIndex from "./routes/index.mjs";
+import connect from "./db/index.mjs";
 
 config();
 const app = express();
@@ -15,5 +16,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/api", apiIndex);
 
 app.listen(PORT, () => {
+	connect();
 	console.log(`Server started at port ${PORT}`);
 });
