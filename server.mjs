6b76362529d20b40626a11ca6d081df55f798a23
Akshat Mittal <akshatmittal2506@gmail.com>
2023-01-04 18:16:16 +0530

    routes setup

--- server.mjs
+++ server.mjs
@@ -3,12 +3,16 @@ import cors from "cors";
 import { config } from "dotenv";
 import { PORT } from "./config/index.mjs";
 
+import apiIndex from "./routes/index.mjs";
+
 config();
 const app = express();
 
 app.use(cors("*"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use("/api", apiIndex);
 
 app.listen(PORT, () => {
 	console.log(`Server started at port ${PORT}`);
