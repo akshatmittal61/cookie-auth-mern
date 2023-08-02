016f3ddfcd8ff20c894f52ccab997f135072f828
Akshat Mittal <akshatmittal2506@gmail.com>
2023-08-02 17:47:07 +0530

    feat: added sample http request

--- controllers/index.mjs
+++ controllers/index.mjs
@@ -1,5 +1,21 @@
-const index = (req, res) => {
-	res.status(200).json({ message: "Hello World!" });
+import { RESPONSE_MESSAGES } from "../constants/enums.mjs";
+
+const index = async (req, res) => {
+	try {
+		const result = await fetch(
+			"https://jsonplaceholder.typicode.com/users"
+		).then((resp) => resp.json());
+		return res.status(200).json({
+			message: RESPONSE_MESSAGES.SUCCESS,
+			data: result,
+		});
+	} catch (error) {
+		console.error(error);
+		return res.status(500).json({
+			message: RESPONSE_MESSAGES.SERVER_ERROR,
+			error: error.message,
+		});
+	}
 };
 
 export { index };
--- src/constants/variables.js
+++ src/constants/variables.js
@@ -1,1 +1,1 @@
-export const backendBaseUrl = process.env.REACT_APP_BACKEND_URL + "/api";
+export const backendBaseUrl = process.env.REACT_APP_BACKEND_URL;
